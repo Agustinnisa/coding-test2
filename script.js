4573ca01796024d3ff0edb9ccd212a3f5808bfe7
Agustinnisa <nisaagustin6@gmail.com>
2024-09-09 20:29:17 +0700

    add function, 15menit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function reverseAndTimeArray(arr) {
-    return arr.reverse().map()
+    let reversedArr = arr.reverse();
+
+    let resultArr = reversedArr.map(num => num * 2);
+    return resultArr;
 }
+
+let array1 = [1, 4, 6, 7];
+let array2 = [7, 4, 2, 1]; 
+let array3 = [5]; 
+
+console.log(reverseAndTimeArray(array1));
+console.log(reverseAndTimeArray(array2));
